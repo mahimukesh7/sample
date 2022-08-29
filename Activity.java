c456da3fc5f112bb4e465c8c309afd61c0ddedf2
mahimukesh7 <111746224+mahimukesh7@users.noreply.github.com>
2022-08-29 15:00:54 +0530

    Add files via upload

---
+++ Activity.java
@@ -0,0 +1,19 @@
+package arra_mukesh_kumar;
+
+public class Activity {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String myStr = "mukesh";
+		
+		String myString = "mukesh loves India.";
+		
+		System.out.println("mystr="+myStr);
+		System.out.println("myString="+myString);
+		
+		String[] myStr1 = new String[3];
+				
+				
+	}
+
+}
